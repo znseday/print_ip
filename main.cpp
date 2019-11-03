2c484be620de7ca77762f0da7e0f97e9a1b1b8b7
znseday <55926888+znseday@users.noreply.github.com>
2019-11-04 02:14:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <list>
 #include <iterator>
 #include <algorithm>
 
@@ -17,9 +17,29 @@ int main()
     // something
 #endif
 
+    MyPrintIp<char>(-1);
+    MyPrintIp<short>(0);
+    MyPrintIp<int>(2130706433);
+    MyPrintIp<__int64>(8875824491850138409);
 
+//    MyPrintIp(2130706433ul); // additional test for unsigned type
 
+//    MyPrintIp((char)-1); // this commented block works as well
+//    MyPrintIp((short)0);
+//    MyPrintIp((int)2130706433);
+//    MyPrintIp((__int64)8875824491850138409);
 
+    string s = "255.255.127.0";
+    MyPrintIp<string>(s);
+    //MyPrintIp(s); // doesn't work without <string> (((
+
+    vector<int> v = {250, 200, 150, 100};
+    //MyPrintIp<vector<int>>(v);
+    MyPrintIp(v);
+
+    list<int> l = {200, 150, 100, 50};
+    //MyPrintIp<list<int>>(l);
+    MyPrintIp(l);
 
     return 0;
 }
--- my_aux.h
+++ my_aux.h
@@ -2,12 +2,13 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
 #include <iterator>
 #include <algorithm>
 #include <utility>
-#include <memory>
+#include <iomanip>
 
-// __FUNCSIG__ is for VS, but Qt (mingw) works with __PRETTY_FUNCTION__
+// __FUNCSIG__ is for VS, but Qt (mingw) works with __PRETTY_FUNCTION__ as well
 #if (defined WIN32) || (defined WIN64)
 //#define MY_P_FUNC __FUNCSIG__
 #define MY_P_FUNC __PRETTY_FUNCTION__
@@ -17,5 +18,86 @@
 
 // I intentionally don't use "std::" because it's complicated code for fisrt TMP experience
 using namespace std;
+
+//template<typename T>
+//void MyPrintIp(T ip) // it's general function, but things aren't so easy(
+//{
+//    throw invalid_argument("ip has incompatible type");
+//}
+
+//template<class T>
+//typename enable_if<is_integral<T>::value, T> void
+//    MyPrintIp(T t)
+//{
+//    std::cout << "integral" << endl;
+//}
+
+//template <typename T>
+//enable_if_t<is_integral_v<T>, void> MyPrintIp(const T ip)
+//{
+//   cout << ip << endl;
+//}
+
+template<class T> // such an idea about unsued parameter is taken from cppreference
+void MyPrintIp(T ip, typename enable_if<is_integral<T>::value>::type* = 0)
+{
+    //cout << dec << ip << endl;           // for debugging
+    //unsigned decltype(ip) mask = 0xff;   // it's logical, but doesn't work (((
+    typename make_unsigned<T>::type mask = 0xff; // this monster works
+    //cout << "mask = " << hex << mask << endl;                 // for debugging
+    //cout << "sizeof(mask) = " << dec << sizeof(mask) << endl; // for debugging
+
+    for (int i = (sizeof(ip)-1)*8; i >= 0; i -= 8)
+        cout << ((ip>>i) & mask) << ((i>0)? "." : "\n");
+}
+
+template<class T>
+void MyPrintIp(const string &ip, typename enable_if<is_class<T>::value>::type* = 0)
+{
+    cout << ip << endl;
+}
+
+//template<class T, class V> // doesn't work for T, but works for 'const vector<V> &ip' (((
+//void MyPrintIp(const vector<V> &ip, typename enable_if<is_class<T>::value>::type* = 0)
+//{
+//    //copy(ip.cbegin(), ip.cend(), ostream_iterator<V>(cout, ".")); // adds extra point at the end (((
+//    for (auto it = ip.cbegin(); it != ip.cend(); ++it)
+//        cout << *it << ((it < (ip.cend()-1))? "." : "\n");
+//}
+
+template<class T, class = typename enable_if<is_class<T>::value>::type >
+void MyPrintIp(T ip)
+{
+    for (auto it = ip.cbegin(); it != ip.cend(); )
+    {
+//      cout << *it << ((it < (ip.cend()-1))? "." : "\n"); // 'it-1' doesn't work for list (((
+        cout << *it++;
+        if (it != ip.cend())
+            cout << ".";
+    }
+    cout << endl;
+}
+
+//template<typename T>  // "error: function template partial specialization is not allowed" ((( !!!!!
+//void MyPrintIp<string>(const string &ip)
+//{   // "error: function template partial specialization is not allowed" ((( !!!!!
+//    cout << ip << endl;
+//}
+
+// something for tuple there...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
